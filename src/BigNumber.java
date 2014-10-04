3cefde5c10b155bb59462e84558380c27450b7bd
Evangileon <yujun.eageon@gmail.com>
2014-10-04 15:47:15 -0500

    docs

--- src/BigNumber.java
+++ src/BigNumber.java
@@ -55,12 +55,15 @@ public class BigNumber {
      */
 	public int divideByInt(String numStr, int intNum, StringBuffer buffer) {
 		String digitMaxStr = String.valueOf(intNum);
+        // The max length of chars that one iteration of division will affect
 		int digitMaxLength = digitMaxStr.length();
 
 		char[] number = numStr.toCharArray();
 		int remnant = 0;
 
         if(number.length < digitMaxLength) {
+            // It number.length, < digitMaxLength, that means
+            // the digit is zero, and the whole number equals to remnant after division
             remnant = charArrayToInt(number, 0, number.length);
             buffer.append("0");
             return remnant;
@@ -80,6 +83,8 @@ public class BigNumber {
                 firstZero = false;
             }
             if(digit != 0 || !firstZero) {
+                // It the first several digit is zero, don't append to result
+                // It just begin at the first non zero digit occurs
                 buffer.append(digit);
             }
 
@@ -96,12 +101,19 @@ public class BigNumber {
 				number[beginDigit + (endDigit - beginDigit) - remnantChars.length + i] = remnantChars[i];
 			}
 
+            // move indices
 			beginDigit += ((endDigit - beginDigit) - remnantChars.length);
 			endDigit += (digitMaxLength - 1); 
 		}
 		return remnant;
 	}
 
+    /**
+     * A string representative number (no limitation) multiplied by a single digit integer
+     * @param numStr string representative number
+     * @param digit single digit integer
+     * @return the result if single digit, otherwise itseld
+     */
     public String multiplyBySingleDigit(String numStr, int digit) {
         if(digit > 9) {
             return numStr;
@@ -115,6 +127,7 @@ public class BigNumber {
         while(i >= 0) {
             int temp = (num[i] - '0') * digit + carry;
             if(temp > 9) {
+                // temp > 9. means there is a carry
                 carry = temp / 10;
                 temp = temp % 10;
             } else {
@@ -127,13 +140,21 @@ public class BigNumber {
         }
 
         if(carry > 0) {
+            // It on the most significant, there is a carry, them the result fit the array completely.
+            // result[0] is used
             result[0] = (char)(carry + '0');
             return new String(result);
         }
 
+        // Otherwise result[0] is never used, truncate it out
         return new String(result, 1, result.length - 1);
     }
 
+    /**
+     * Collect every digit of an integer, and store them into List, by the order of significance
+     * @param num non-negative integer
+     * @param digits List to store digits
+     */
     private void collectDigits(int num, ArrayList<Integer> digits) {
         if(num / 10 > 0) {
             collectDigits(num / 10, digits);
@@ -142,33 +163,47 @@ public class BigNumber {
     }
 
     public String multiplyByInt(String numStr, int intNum) {
+        // get every digit of intNum
         ArrayList<Integer> digits = new ArrayList<Integer>();
         collectDigits(intNum, digits);
 
         String result = "0";
         for (int i = 0; i < digits.size(); i++) {
             if(!result.equals("0")) {
+                // if result != "0", multiplied by 10, that is appended a "0"
                 result = result + "0";
             }
 
             String temp = multiplyBySingleDigit(numStr, digits.get(i));
+            // result * 10 + temp
             result = addTwoNumStr(result, temp);
         }
 
         return result;
     }
 
+    /**
+     * Return the result of the sum of two char array representative numbers
+     * @param array1 left operand
+     * @param array2 right operand
+     * @return result
+     */
     public String addTwoNumStr(char[] array1, char[] array2) {
         int i = array1.length - 1;
         int j = array2.length - 1;
 
+        // The size of the result should be the max of two length,
+        // plus one digit for most significant carry, if needed.
         char[] result = new char[Math.max(i, j) + 2];
         int k = result.length - 1;
-        int carry = 0;
+        int carry = 0; // initialized to 0
 
+        // For tail to head
         while(i >= 0 || j >= 0) {
+            // If i and exceed the range of two arrays, let them be zero
             char temp1 = (i >= 0) ? array1[i] : '0';
             char temp2 = (j >= 0) ? array2[j] : '0';
+            // digit of array1 + digit of array2 + carry
             int tmpResult = (temp1 - '0') + (temp2 - '0') + carry;
             if(tmpResult > 9) {
                 carry = 1;
@@ -183,32 +218,52 @@ public class BigNumber {
         }
 
         if(carry == 1) {
+            // It on the most significant, there is a carry, them the result fit the array completely.
+            // result[0] is used
             result[0] = (char)(1 + '0');
             return new String(result);
         }
 
+        // Otherwise result[0] is never used, truncate it out
         return new String(result, 1, result.length - 1);
     }
 
+    /**
+     * Return the result of the sum of two string representative numbers, actually it's a wrapper of
+     * String addTwoNumStr(char[] array1, char[] array2)
+     * @param num1 left operand
+     * @param num2 right operand
+     * @return result
+     */
     public String addTwoNumStr(String num1, String num2) {
         char[] array1 = num1.toCharArray();
         char[] array2 = num2.toCharArray();
         return addTwoNumStr(array1, array2);
     }
-	
+
+    /**
+     * String to big number
+     * @param str the string representative decimal
+     */
 	public void strToNum(String str) {
 		int remnant;
         String num = str;
 
         while(!num.equals("0")) {
+            // If num = "0", means the result of division is 0, so the loop ends
             StringBuffer buffer = new StringBuffer();
             remnant = divideByInt(num, this.base, buffer);
             num = buffer.toString();
             //System.out.println(num);
+            // remnant is the content of every node
             digitList.add(remnant);
         }
 	}
 
+    /**
+     * Big number to string
+     * @return the string representative decimal
+     */
     public String numToStr() {
         Iterator<Integer> itor = this.digitList.descendingIterator();
         String result = "0";
@@ -217,10 +272,13 @@ public class BigNumber {
             String temp = String.valueOf(digit);
 
             if(itor.hasNext()) {
+                // escape last one, so that the result be correct
                 // you should choose base wisely so that the operation won't overflow
                 result = multiplyByInt(result, this.base);
+                // multiplied by base
                 temp = String.valueOf(this.base * digit);
             }
+            // add result * base + temp
             result = addTwoNumStr(result, temp);
         }
 
